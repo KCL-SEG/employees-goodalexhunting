0343792944fede3484e17eb4afe7c269d9dd3a11
Alex San <goodalexhunting@gmail.com>
2022-09-30 13:48:35 +0100

    brute#3

--- employee.py
+++ employee.py
@@ -22,13 +22,19 @@ class Salaried(Employee):
 
     def get_pay(self):
         return self.salary
-
+    def __str__(self):
+        return f"{self.name} works on a monthly salary of {self.salary}.  Their total pay is {self.salary}."
+    # Billie works on a monthly salary of 4000.  Their total pay is 4000.
 class Sal_Commission(Salaried):
     def __init__(self, name, salary = None, contracts = None, contract_rate = None):
         super().__init__(name, salary)
-        self.commission = contracts * contract_rate
+        self.contracts = contracts
+        self.contract_rate = contract_rate
     def get_pay(self):
-        return self.salary + self.commission
+        return self.salary + (self.contracts * self.contract_rate)
+    def __str__(self):
+        return f"{self.name} works on a monthly salary of {self.salary} and receives a commission for {self.contracts} contract(s) at {self.contract_rate}/contract.  Their total pay is {self.get_pay()}."
+
 class Sal_Bonus(Salaried):
     def __init__(self, name, salary = None, bonus = None):
         super().__init__(name, salary)
@@ -36,15 +42,22 @@ class Sal_Bonus(Salaried):
 
     def get_pay(self):
         return self.salary + self.bonus
+    
+    def __str__(self):
+        return f"{self.name} works on a monthly salary of {self.salary} and receives a bonus commission of {self.bonus}.  Their total pay is {self.get_pay()}."
+
 
 class Hourly(Employee):
     def __init__(self, name, hour_rate = None, hours = None):
         super().__init__(name)
-        self._hour_rate = hour_rate
-        self._hours = hours
+        self.hour_rate = hour_rate
+        self.hours = hours
 
     def get_pay(self):
-        return self._hour_rate * self._hours
+        return self.hour_rate * self.hours
+
+    def __str__(self):
+        return f'{self.name} works on a contract of {self.hours} hours at {self.hour_rate}/hour.  Their total pay is {self.get_pay()}.'
 
 class Hr_Bonus(Hourly):
     def __init__(self, name, hour_rate = None, hours = None, bonus = None):
@@ -52,32 +65,41 @@ class Hr_Bonus(Hourly):
         self.bonus = bonus
 
     def get_pay(self):
-        return (self._hour_rate * self._hours) + self.bonus
+        return (self.hour_rate * self.hours) + self.bonus
+    def __str__(self):
+        return f'{self.name} works on a contract of {self.hours} hours at {self.hour_rate}/hour and receives a bonus commission of {self.bonus}.  Their total pay is {self.get_pay()}.'
+
 
 class Hr_Commission(Hourly):
     def __init__(self, name, hour_rate = None, hours = None, contracts = None, contract_rate = None):
         super().__init__(name, hour_rate, hours)
+        self.contracts = contracts
+        self.contract_rate = contract_rate
         self.commission = contracts * contract_rate
 
     def get_pay(self):
-        return (self._hour_rate * self._hours) + self.commission
+        return (self.hour_rate * self.hours) + self.commission
+
+    def __str__(self):
+        return f"{self.name} works on a contract of {self.hours} hours at {self.hour_rate}/hour and receives a commission for {self.contracts} contract(s) at {self.contract_rate}/contract.  Their total pay is {self.get_pay()}."
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
 billie = Salaried('Billie', salary = 4000)
-print(billie.get_pay()) 
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
 charlie = Hourly('Charlie', hour_rate = 25, hours = 100)
+print(str(charlie))
 # print(charlie.get_pay())
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
 renee = Sal_Commission('Renee', salary = 3000, contract_rate=200, contracts=4)
+print(str(renee))
 # print(renee.get_pay())
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
 jan = Hr_Commission('Jan', hours = 150, hour_rate = 25, contracts = 3, contract_rate= 220)
-# print(jan.get_pay())
+print(str(jan))
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
 robbie = Sal_Bonus('Robbie', salary = 2000, bonus=1500)
-# print(robbie.get_pay())
+
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = Hr_Bonus('Ariel', hour_rate=30, hours = 120, bonus = 600)
-# print(ariel.get_pay())
+
